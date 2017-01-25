0de1fe41350e285eb6b89df517a7a3b666263b45
root <root@localhost.localdomain>
2017-01-25 17:57:11 -0500

    geting full site info is available

--- tools/getinfo.sh
+++ tools/getinfo.sh
@@ -1,12 +1,12 @@
+echo -e "--------------SITE MAP-------------\n" 
 wget --spider --recursive --no-verbose --output-file=crawler.txt $1
-printf "--------------SITE MAP-------------\n" 
 sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" crawler.txt | sed "s@&@\&amp;@" 
-printf "\n\n--------------INFORMATION----------\n" 
+echo -e "\n\n--------------INFORMATION----------\n" 
 nslookup -q=ns $1 
-printf "\n\n--------WHOIS INFO-----------\n" 
+echo -e "\n\n--------WHOIS INFO-----------\n" 
 whois $1 
-printf "\n\n--------ROBOTS.TXT-----------\n"
+echo -e "\n\n--------ROBOTS.TXT-----------\n"
 curl $1/robots.txt 
-printf "\n\n--------NMAP SCANNING--------\n" 
+echo -e "\n\n--------NMAP SCANNING--------\n" 
 nmap -T4 -sS -n $1 
 
