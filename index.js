ced045690cadd6d387ed4c5209624cd7e99bcba2
Prifki <yar95b@ya.ru>
2017-01-26 02:35:00 +0300

    Going on

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/info"] = requestHandlers.info;
+handle["/injections"] = requestHandlers.injections;
 
 server.start(router.route, handle);
--- tools/getparams.sh
+++ tools/getparams.sh
@@ -1,3 +1,3 @@
-curl $(cat sitemap) | sed '1,/<form/d' | sed -n 's/.*name="\([^"]*\).*/\1/p'
+curl $1 | sed '1,/<form/d' | sed -n 's/.*name="\([^"]*\).*/\1/p'
 
 
