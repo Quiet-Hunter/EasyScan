6aba50ee28f682b700c0b6bed222b3c877e135e2
Prifki <yar95b@ya.ru>
2017-03-22 23:09:42 +0300

    AWESOME XSS ATTACKS!!!

--- tools/getinfo.sh
+++ tools/getinfo.sh
@@ -1,13 +1,13 @@
-echo -e "--------------SITE MAP-------------\n" 
+echo -e "<br><h4>SITE MAP</h4>" 
 wget --spider --recursive --no-verbose --output-file=crawler.txt $1
-sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" crawler.txt | sed "s@&@\&amp;@" 
+sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" crawler.txt | sed "s@&@\&amp;@" | sed 's/$/ <br>/'
 rm crawler.txt
-echo -e "\n\n--------------INFORMATION----------\n" 
-nslookup -q=ns $1 
-echo -e "\n\n--------WHOIS INFO-----------\n" 
-whois $1 
-echo -e "\n\n--------ROBOTS.TXT-----------\n"
-curl $1/robots.txt 
-echo -e "\n\n--------NMAP SCANNING--------\n" 
-nmap -T4 -sS -n $1 
+echo -e "<br><h4>DNS INFO</h4>" 
+nslookup -q=ns $1 | sed 's/$/ <br>/'
+echo -e "<br><h4>WHOIS INFO</h4>" 
+whois $1 | sed 's/$/ <br>/'
+echo -e "<br><h4>ROBOTS.TXT</h4>"
+curl $1/robots.txt | sed 's/$ <br>/'
+echo -e "<br><h4>NMAP SCANNING</h4>" 
+nmap -T4 -sS -n $1 | sed 's/$/ <br>/'
 rm -rf $1
